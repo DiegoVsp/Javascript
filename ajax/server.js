62e191c928d9bf3a9922b3c5aad8f054e6d27ca9
Your Name <diego-vespa2020@outlook.com.br>
2020-06-16 12:07:10 -0300

    finalizando seção 14

--- ajax/server.js
+++ ajax/server.js
@@ -27,4 +27,25 @@ app.post("/upload", (req, res) =>{
     res.end('Concluido com sucesso.')
   })
 })
+
+app.post('/formulario', (req,res) => {
+  res.send({
+    ...req.body,
+    id: 7
+  })
+}) 
+
+app.get('/parOuImpar', (req, res) => {
+  // req.body
+  // req.query
+  // req.params
+  const par = parseInt(req.query.numero) % 2 === 0
+  res.send({
+    resultado: par? 'par' : 'impar'
+  })
+})
+
+
 app.listen(8081, () => console.log('Executando...'))
+
+
