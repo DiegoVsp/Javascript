44779611b22c5991da76420a4972a9a6bf5c9692
Your Name <diego-vespa2020@outlook.com.br>
2020-06-29 10:31:35 -0300

    finalizando capitulo gulp

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,21 +1,40 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
 function appHTML(cb) {
-
-  return cb()
+  return gulp.src('src/**/*.html') /*pega todos os aqruivos da pasta src que tenham final .html*/
+  .pipe(htmlmin({collapseWhitespace:true}))
+  .pipe(gulp.dest('build'))
 }
-function appCSS(cb) {
+function appCSS() {
 
-  return cb()
+  return gulp.src('src/assets/sass/index.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(uglifycss({"uglyComments": true})) /*Para não colocar os comentários no arquivo final*/
+  .pipe(concat("app.min.css"))
+  .pipe(gulp.dest("build/assets/css"))
 }
-function appJS(cb) {
+function appJS() {
+  return gulp.src('src/assets/js/**/*.js')
+  .pipe(babel({presets: ['ENV'] }))
+  .pipe(uglify())
+  .pipe(concat('app.min.js'))
+  .pipe(gulp.dest('build/assets/js'))
+}
+function appIMG() {
+  return gulp.src('src/assets/imgs/**/*.*')
+  .pipe(gulp.dest('build/assets/imgs'))
+}
 
-  return cb()
-}
-function appIMG(cb) {
-
-  return cb()
-}
+gulp.task('appHTML', appHTML)
+gulp.task('appCSS', appCSS)
+gulp.task('appJS', appJS)
+gulp.task('appIMG', appIMG)
 
 module.exports = {
   appHTML,
