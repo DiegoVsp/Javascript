01e372990f73273294f1a6ba465e27e50467c455
Your Name <diego-vespa2020@outlook.com.br>
2020-07-06 16:00:02 -0300

    atualizando aulas webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,45 @@
+const modoDev = process.env.NODE_DEV !== 'production'
 const webpack = require('webpack')
+const MiniExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  mode:'development',
-  entry: './src/principal.js'
+  mode: modoDev ? 'development' : 'production',
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]  
+  },
+  plugins: [
+    new MiniExtractPlugin({
+      filename: "estilo.css"
+    }),
+    new TerserPlugin({
+      parallel:true,
+      terserOptions: {
+        ecma: 6,
+      },
+    }),
+  ],
+  module: {
+    rules: [{
+      // test: /\.css$/,
+      test: /\.s?[ac]ss$/,
+      use: [
+        MiniExtractPlugin.loader, //Extract css into files
+        //'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+        'css-loader', //interpreta @import, url()
+        'sass-loader'
+      ]
+    }, {
+      test:/\.(png|svg|jpg|gif)$/,
+      use: ['file-loader']
+    }]
+  }
 }
