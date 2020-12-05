82f39d8509bf331f9d8436901b62a8752343fd9f
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-12-05 18:07:43 -0300

    att

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
 // const elemento = <h1>React 4</h1>
 
 ReactDOM.render(
   
-  <Primeiro/>,
+  <BomDia nome = "Diego Vespa" idade = {29}/>,
   document.getElementById('root')
 )
