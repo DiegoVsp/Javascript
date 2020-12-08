bcfab8fc58d434d3dcddbc3a0bbd90c873f24749
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-12-08 18:59:02 -0300

    att

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 
 // import Multi, { BoaNoite } from './componentes/Multiplos';
 // import BomDia from './componentes/BomDia'
-import Saudacao from './componentes/Saudacao';
+// import Saudacao from './componentes/Saudacao';
+import Pai from './componentes/Pai';
 
 // const elemento = <h1>React 4</h1>
 
 ReactDOM.render(
 
   <div>
-    <Saudacao tipo="Bom dia" nome="Francisca Silva" />
+    <Pai nome="Diego" sobrenome="Vespa"/>
+    {/* <Saudacao tipo="Bom dia" nome="Francisca Silva" /> */}
     {/* <Multi.BoaTarde nome="Diego Vespa" />
     <Multi.BoaNoite nome="Francisca"/> */}
   </div>,
